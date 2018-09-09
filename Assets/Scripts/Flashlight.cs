e868b182ab9fedc7535f09a21ee9e67894d97b93
miguelmcell <miguelmendoza1693@yahoo.com>
2018-09-08 21:51:57 -0500

    stuff

--- Assets/Scripts/Flashlight.cs
+++ Assets/Scripts/Flashlight.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Flashlight : MonoBehaviour {
-
+    Light flashlight;
 	// Use this for initialization
 	void Start () {
-		
+        flashlight = GetComponent<Light>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            flashlight.enabled = !flashlight.enabled;
+        }
 	}
 }
