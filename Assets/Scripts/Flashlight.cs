c3db1f098ff41926da16391ace40ce3d517d5b39
Bill-Nye777 <alex.mendoza.mondragon7777777@gmail.com>
2018-09-08 22:45:08 -0700

    Finished Flashlight,lel

---
+++ Assets/Scripts/Flashlight.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flashlight : MonoBehaviour
+{
+    bool lightbutto = true;
+    public Light flashlight;
+
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.R) && (flashlight.enabled == true))
+        {
+            flashlight.enabled = false;
+        }
+        if(Input.GetKey(KeyCode.R)&&(flashlight.enabled==false)){
+            flashlight.enabled = true;
+        }
+    }
+}
