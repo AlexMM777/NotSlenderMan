6b0939284651ffc74302d1828f9dbff8bd81b245
miguelmcell <miguelmendoza1693@yahoo.com>
2018-11-21 09:48:36 -0600

    fixed rain

---
+++ Assets/Scripts/Dragon.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dragon : MonoBehaviour {
+    float timer;
+    float angle;
+    public float rad;
+    public GameObject center;
+	// Use this for initialization
+	void Start () {
+        timer = 0;
+        angle = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime * 0.4f;
+        angle = timer;
+        this.transform.position = new Vector3((center.transform.position.x + Mathf.Sin(angle) * rad), center.transform.position.y, ((center.transform.position.z + Mathf.Cos(angle) * rad)));
+        transform.Rotate(Vector3.up * Time.deltaTime * 15);
+    }
+}
